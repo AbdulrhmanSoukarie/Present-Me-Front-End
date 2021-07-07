ad740dcac764f6783cf1c23b5ed41112ddfa446f
Omar Chouman <omar.chouman0@gmail.com>
2021-07-07 19:37:17 +0300

    Navbar without routing

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Navbar.css";
+import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+const navbar = () => {
+    return (
+        <div>
+        <input type="checkbox" id="check" />
+            <nav>
+                <div className="icon">Present Me</div>
+                <div className="search">
+                    <input type="search" placeholder="Search here..." />
+                    <SearchIcon style={{fontSize: 30}} className="search_material" />
+                </div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Discover</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+                <label htmlFor="check" className="bar">
+                    <MenuIcon style={{ color: '#fff' }} id="bars"/>
+                    <CloseIcon style={{ color: '#fff' }} id="times"/>
+                </label>
+            </nav>
+        </div>
+    )
+}
+
+export default navbar
