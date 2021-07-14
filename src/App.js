2a4dc1420c20e5a1d5aaec7cbdb0a5d4c71a6ef1
abdulrhman soukarieh <abdulrhman.soukarieh1@gmail.com>
2021-07-14 12:04:46 +0300

    Categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Sliders from "./component/slider/Sliders"
+import Categories from "./component/categories/Categories"
 function App() {
   return (
     <div className="App">
     <Sliders></Sliders>
+    <Categories></Categories>
     </div>
   );
 }
