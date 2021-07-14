9ea75d8d20768f6f2722810c683d667482b2d0f1
abdulrhman soukarieh <abdulrhman.soukarieh1@gmail.com>
2021-07-14 12:19:45 +0300

    title

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Sliders from "./component/slider/Sliders"
 import Categories from "./component/categories/Categories"
+import Title from './component/titles/Title';
 function App() {
   return (
     <div className="App">
     <Sliders></Sliders>
+    <Title title="My Category"></Title>
     <Categories></Categories>
     </div>
   );
---
+++ src/component/titles/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../titles/title.css'
+function Tittle({title}) {
+    return (
+        <div className="title">
+            <h1 >{title}</h1>
+        </div>
+    )
+}
+
+export default Tittle
