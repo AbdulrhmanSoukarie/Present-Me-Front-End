06d51d669c25d18d5fc96c5ba8208fe05952defd
Omar Chouman <omar.chouman@hotmail.com>
2021-07-14 10:50:42 +0300

    Single Event Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
 import './App.css';
+import SingleEvent from "./components/singleEvent/SingleEvent";
+import eventImg from "./components/singleEvent/event-img.png";
+import hostImg from "./components/singleEvent/host-image.png";
 
 function App() {
   return (
     <div className="App">
 
-      <h1>Hello shbeb</h1>
+      <SingleEvent
+        title = "Algebra"
+        eventimg = {eventImg}
+        description = "Prepare for algebra"
+        date = "7/7/2021"
+        hostimg = {hostImg}
+        hostname = "Omar Chouman"
+       />
     
     </div>
   );
---
+++ src/components/singleEvent/SingleEvent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./SingleEvent.css";
+
+
+const SingleEvent = (props) => {
+    return (
+        <div className="container">
+        <h1>{props.title}</h1>
+        <img src={props.eventimg} className="event__img" alt=""/>
+        <div className="box">
+          <div className="event-info">
+            <h3>Title: {props.title}</h3>
+            <h3>Description: {props.description}</h3>
+            <h3>Event Date: {props.date}</h3>
+          </div>
+          <div className="host-info">
+            <img src={props.hostimg} alt=""/>
+            <h3>{props.hostname}</h3>
+          </div>
+        </div>
+        <button className="btn">Attend</button>
+        </div>
+    )
+}
+
+export default SingleEvent
