861cd1aef25bac4dc989f5d7089dbb88216680dc
Omar Chouman <omar.chouman0@gmail.com>
2021-07-15 12:25:34 +0300

    All Main Categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import MainCategories from "./components/allMainCategories/AllCategories";
 
 function App() {
   return (
     <div className="App">
 
-      <h1>Hello shbeb</h1>
+      <MainCategories />
     
     </div>
   );
---
+++ src/components/allMainCategories/AllCategories.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./AllCategories.css";
+import Education from "./images/education.png";
+import Music from "./images/music.png";
+import Technology from "./images/technology.png";
+
+
+const Categories = [
+    {
+        title: "Education",
+        image: Education
+    },
+    {
+        title: "Music",
+        image: Music
+    },
+    {
+        title: "Technology",
+        image: Technology
+    },
+    {
+        title: "Education",
+        image: Education
+    },
+    {
+        title: "Music",
+        image: Music
+    },
+    {
+        title: "Technology",
+        image: Technology
+    }
+];
+
+const allCategories = () => {
+    return ( 
+    <div >
+        <div className="container">
+            {Categories.map( (val, index) => (
+                <div key={index} className="each-category">
+                    <div className="category-img" style={{ 'backgroundImage': `url(${val.image})`}}>
+                        <button className="btn">{val.title}</button>
+                    </div>
+                </div>
+            ) )}
+        </div>  
+    </div>
+    )
+}
+
+export default allCategories
